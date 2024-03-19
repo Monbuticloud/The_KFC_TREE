63bcde63e54cc76da3ba590a91d67dddf5e3f232
Monbuticloud <145574857+Monbuticloud@users.noreply.github.com>
2024-03-19 18:26:12 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,15 +4,15 @@ addLayer("?", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(1),
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Souls", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.001, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -22,7 +22,7 @@ addLayer("?", {
     },
     row: "side", // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "S", description: "???", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
@@ -45,14 +45,25 @@ addLayer("s", {
     exponent: 0.9, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+	    if (hasUpgrade('p', 11)) gain = gain.times(10)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1.1)
     },
-    row: "side", // Row the layer is in on the tree (0 is the first row)
+    row: "0", // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "M", description: "M: Sell your KFC to inprove your store", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
+	   upgrades: {
+		           11: {
+    title: "More chicken",
+    description: "10x your Ckicken gain.",
+    cost: new Decimal(10),
+
+        },
+
+    },
+
 })
